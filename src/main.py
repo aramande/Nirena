ecaacc73b79b44bb08dd76de0159fd015f9feb28
unknown <Zack@.(none)>
2010-08-24 21:12:56 +0200

    Added a window and a close method.

--- src/main.py
+++ src/main.py
@@ -1,2 +1,14 @@
-#!/usr/bin/env python
-print("Hello world!")
+#!/usr/bin/python
+
+import sys
+import pygame
+
+pygame.init()
+
+pygame.display.set_caption('Project 13')
+size = width, height = 600,400
+screen = pygame.display.set_mode(size)
+
+while 1:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:sys.exit()
