f677347c46949d5d03413dca85b3ebf095fd9f27
Intugo <intugo@hackular.com>
2010-08-25 03:26:23 +0200

    Added an fpsrate of 60 fps.

--- src/main.py
+++ src/main.py
@@ -2,13 +2,21 @@
 
 import sys
 import pygame
+import pygame.mixer
 
 pygame.init()
 
+#variables
+clock = pygame.time.Clock()
+
+#window
 pygame.display.set_caption('Project 13')
 size = width, height = 640,480
 screen = pygame.display.set_mode(size)
 
+#loop
 while 1:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:sys.exit()
+#fps        
+        clock.tick(60)
